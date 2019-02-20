5c4211631e2fb909f0e5894f727350c910652d5d
minagawah <minagawa@tokyo800.jp>
2019-02-20 12:55:36 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # debounce-ctx
-Simple debounce utility.
+
+Simple debounce utility (optionally takes a specific context).
+
+## Install
+
+```
+npm install debounce-ctx
+```
+
+## Usage
+
+```
+<script src="debounce-ctx.min.js"></script>
+<script>
+document.body.addEventListener('resize', debounce(resize, 400));
+</script>
+```
+
+or
+
+```
+const debounce = require('debounce-ctx');
+document.body.addEventListener('resize', debounce(resize, 400));
+```
+
+If you want to bind the function to a certain context,
+you can either simply bind it:
+
+```
+var ctx = new MyClass();
+document.body.addEventListener('resize', debounce(resize.bind(ctx), 400));
+```
+
+or, you can give the context as the third argument:
+
+```
+var ctx = new MyClass();
+document.body.addEventListener('resize', debounce(resize.bind(ctx), 400, ctx));
+```
+
+## License
+
+MIT License  
+Copyright (c) 2019 Hiroki Minagawa
+
+See [LICENSE](./LICENSE) for details.
---
+++ src/index.js
@@ -0,0 +1,36 @@
+/**
+ * @param {Function} [f] Function to apply debounce.
+ * @param {number} [wait] Time you want to suppress.
+ * @param {Object} [ctx] Context to bind the function.
+ * @returns {Function}
+ */
+function debounce (f, wait, ctx) {
+  if (typeof f !== 'function') {
+    throw new Error('No function is given');
+  }
+  if (typeof wait === 'undefined') {
+    wait = 400;
+  }
+  if (typeof ctx === 'undefined') {
+    ctx = null;
+  }
+  var timeout = null;
+  var args = null;
+  var g = function () {
+    (typeof Reflect === 'object') ?
+      Reflect.apply(f, ctx, args) :
+      Function.prototype.apply.call(f, ctx, args);
+  };
+  return function () {
+    ctx = ctx || this || {}; // Use the given context (if there is one).
+    args = arguments;
+    if (timeout) {
+      clearTimeout(timeout);
+    } else {
+      g();
+    }
+    timeout = setTimeout(g, wait);
+  };
+}
+  
+global.debounce = module.exports = debounce;
